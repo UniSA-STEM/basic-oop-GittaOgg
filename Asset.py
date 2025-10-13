faef61e7b38633434a7bb1b46f48a8cd04079b62
GittaOgg <tashhunterisme@gmail.com>
2025-10-13 13:46:28 +0800

    Rough draft of init and str for asset class

--- Asset.py
+++ Asset.py
@@ -1,8 +1,27 @@
 """
 File: Asset.py
 Description: <A brief description of this Python module.>
-Author: <full name>
-ID: <student_id>
-Username: <username>
+Author: Natasha Hunter
+ID: 110439590
+Username: hunny006
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+class Asset():
+
+    def __init__(self, name, description, encrypted =False):
+        self.name = name
+        self.description = description
+        self.encrypted = encrypted
+
+    def __str__(self):
+        if self.encrypted==True:
+            return f'**********\n<{self.name}>: <{self.description}> [Encrypted]\n**********\n'
+        else:
+            return f'**********\n<{self.name}>: <{self.description}>\n**********\n'
+
+
+
+
+spike1 = Asset("DataSpike", "data spike", True)
+print(spike1)
