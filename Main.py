38657d3973fde0ac5fa83f247a8220740bed96df
GittaOgg <tashhunterisme@gmail.com>
2025-10-13 14:01:25 +0800

    Initial setup of main- importing classes and testing of instantiating

--- Main.py
+++ Main.py
@@ -1,8 +1,27 @@
 """
 File: main.py
 Description: <A brief description of this Python module.>
-Author: <full name>
-ID: <student_id>
-Username: <username>
+Author: Natasha Hunter
+ID: 110439590
+Username: hunny006
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+import Hacker
+import Rig
+import Asset
+
+
+print(Asset.Asset('A1','cryptotoken'))
+print(Asset.Asset('A2','Security_Chip',True))
+
+
+print(Rig.Rig("Rigmarole"))
+print(Rig.Rig("TheRigApple"))
+
+
+
+print(Hacker.Hacker('Hackpanther'))
+print(Hacker.Hacker('Hackula'))
+
+
